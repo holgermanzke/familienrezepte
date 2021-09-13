ebdce65e5ec5e0f76aa6fb5e7aef651852a94f04
holgermanzke <manzkeholger@gmail.com>
2021-09-13 21:29:34 +0200

    Add RecipeController

---
+++ app/controllers/recipe_controller.rb
@@ -0,0 +1,31 @@
+class RecipeController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:notice] = "Rezept wurde erfolgreich angelegt"
+      redirect_to recipes_path
+    else
+      flash[:error] = "Upps... das Rezept konnte nicht gespeichert werden."
+      render "new"
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
+
+end
