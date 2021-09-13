6a6632243989612ce8272305f6c1cddf8518bd78
holgermanzke <manzkeholger@gmail.com>
2021-09-13 21:41:37 +0200

    Fix typo in column name

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_09_13_185228) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
-    t.text "descriptin"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
