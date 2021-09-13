3fce52174076d74ab4446e3238385255d2cab329
holgermanzke <manzkeholger@gmail.com>
2021-09-13 22:53:20 +0200

    Create seeds for ingredient units

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ingredient_units = IngredientUnit.create([{ name: "Kilogramm", shortcut: "kg"}, { name: "Milligramm", shortcut: "mg"}, { name: "Gramm", shortcut: "g"}, { name: "Pfund", shortcut: "pfd"}, { name: "Milliliter", shortcut: "ml"}, { name: "Centiliter", shortcut: "cl"}, { name: "Deziliter", shortcut: "dl"}, { name: "Liter", shortcut: "l"}, { name: "Tropfen", shortcut: "Tr"}, { name: "Spritzer", shortcut: "Sp"}, { name: "Schuss", shortcut: "schuss"}, { name: "Prise", shortcut: "Pr"}, { name: "Messerspitze", shortcut: "Msp."}, { name: "Teelöffel", shortcut: "TL"}, { name: "Esslöffel", shortcut: "EL"}, { name: "Tasse", shortcut: "Tas"}, { name: "Bunde", shortcut: "Bd"}, { name: "Scheibe", shortcut: "Sc"}, { name: "Stück", shortcut: "Stk."}])
