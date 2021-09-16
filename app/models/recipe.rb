0847541df9cce2441476620be79af07cbc3d7471
holgermanzke <manzkeholger@gmail.com>
2021-09-16 22:52:50 +0200

    Add user reference to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy, inverse_of: :recipe
+  belongs_to :user
   validates :title, :description, presence: true
 
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_203833) do
+ActiveRecord::Schema.define(version: 2021_09_16_191218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2021_09_15_203833) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2021_09_15_203833) do
 
   add_foreign_key "ingredients", "ingredient_units"
   add_foreign_key "ingredients", "recipes"
+  add_foreign_key "recipes", "users"
 end
